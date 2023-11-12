37f8ce2f6d6bf48f412ab5bebfd17f455a4d37a0
bekzoddr <sodiqov.bekzoddr@gmail.com>
2023-11-12 23:11:40 +0500

    figma

---
+++ js/dark-light.js
@@ -0,0 +1,10 @@
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.gif";
+  }
+  document.body.classList.toggle("light");
+});
---
+++ js/loading.js
@@ -0,0 +1,14 @@
+// const loading = document.getElementById("loading");
+
+// const loadingDuration = 3000; // 3s
+
+// setTimeout(() => {
+//   loading.classList.add("loading-none");
+// }, loadingDuration);
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2000; // 2s
+
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, loadingDuration);
